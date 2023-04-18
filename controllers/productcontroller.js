705a17ea97cfd740d2ddf4b4d89d79dc5ba4c42a
harpreet997 <harpreet97@gmail.com>
2023-04-18 15:42:12 +0530

    Remove the image property from product controller

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -13,18 +13,18 @@ const addProduct = async (req, res) => {
         //     arrImages[i] = path.join(newPath, req.files[0].filename);
         // }
 
-        const imagePath = path.join(newPath, req.file.filename)
-        const newPath1 = `https://node-product-management.vercel.app/products/public/assets/`
-        var q = url.parse(newPath1, true);
-        console.log(q.host)
-        const newPath2 = `https://${q.host}/products/public/assets/${req.file.filename}`
-        console.log(imagePath)
+        // const imagePath = path.join(newPath, req.file.filename)
+        // const newPath1 = `https://node-product-management.vercel.app/products/public/assets/`
+        // var q = url.parse(newPath1, true);
+        // console.log(q.host)
+        // const newPath2 = `https://${q.host}/products/public/assets/${req.file.filename}`
+        // console.log(imagePath)
         var product = new ProductModal({
             productname: req.body.productname,
             rating: req.body.rating,
             price: req.body.price,
             description: req.body.description,
-            images: newPath2
+            // images: newPath2
         })
 
         const productData = await product.save();
